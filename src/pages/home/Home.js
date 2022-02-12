3b2e38957127856a41fb9d82871b34f35e9f03fa
naerim <naerim1119@gmail.com>
2022-02-12 12:54:07 +0900

    use rechart

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,17 @@
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
 import FeaturedInfo from "../../featuredInfo/FeaturedInfo";
 import "./home.css";
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        title="User Analytics"
+        data={userData}
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
