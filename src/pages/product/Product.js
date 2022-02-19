f665c18e6a66d56dc1fb2500b51a97e4c08b6280
naerim <naerim1119@gmail.com>
2022-02-20 00:07:26 +0900

    new Product page

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -9,7 +9,7 @@ export default function Product() {
     <div className="product">
       <div className="productTitleContainer">
         <h1 className="productTitle">Product</h1>
-        <Link to="newProducts">
+        <Link to="/newProduct">
           <button className="productAddButton">Create</button>
         </Link>
       </div>
