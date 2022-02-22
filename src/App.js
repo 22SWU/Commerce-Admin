297ee4e9da19066b53bca9b4d2bd70de55e9f6ee
naerim <naerim1119@gmail.com>
2022-02-22 23:13:41 +0900

    outlet 사용해서 layout 설정

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
+
+const Layout = () => (
+  <div>
+    <Topbar />
+    <div className="container">
+      <Sidebar />
+      <Outlet />
+    </div>
+  </div>
+);
 
 function App() {
   return (
     <BrowserRouter>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
           <Route path="/user/:userId" element={<User />} />
@@ -24,8 +34,8 @@ function App() {
           <Route path="/products" element={<ProductList />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="/newProduct" element={<NewProduct />} />
-        </Routes>
-      </div>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
