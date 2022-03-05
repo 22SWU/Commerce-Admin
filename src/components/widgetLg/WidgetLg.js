f787048880b42f7fd363c0499843876b049d0c72
naerim <naerim1119@gmail.com>
2022-03-05 21:36:44 +0900

    fetch newUser, newOrder

--- src/components/widgetLg/WidgetLg.js
+++ src/components/widgetLg/WidgetLg.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { format } from "timeago.js";
+import { userRequest } from "../../requestMethod";
 import "./widgetLg.css";
 
 export default function WidgetLg() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await userRequest.get("orders");
+        setOrders(res.data);
+      } catch {}
+    };
+    getOrders();
+  }, []);
+
   const Button = ({ type }) => {
     return <button className={"widgetLgButton " + type}>{type}</button>;
   };
+
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
@@ -15,66 +30,18 @@ export default function WidgetLg() {
             <th className="widgetLgTh">Amount</th>
             <th className="widgetLgTh">Status</th>
           </tr>
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://i.ibb.co/VH6Hstf/gary-bendig-6-GMq7-AGx-Nb-E-unsplash.jpghttps://i.ibb.co/fS1D105/jonatan-pie-xg-TMSz6keg-E-unsplash.jpghttps://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Approved" />
-            </td>
-          </tr>
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://i.ibb.co/VH6Hstf/gary-bendig-6-GMq7-AGx-Nb-E-unsplash.jpghttps://i.ibb.co/fS1D105/jonatan-pie-xg-TMSz6keg-E-unsplash.jpghttps://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Declined" />
-            </td>
-          </tr>
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://i.ibb.co/VH6Hstf/gary-bendig-6-GMq7-AGx-Nb-E-unsplash.jpghttps://i.ibb.co/fS1D105/jonatan-pie-xg-TMSz6keg-E-unsplash.jpghttps://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Pending" />
-            </td>
-          </tr>
-          <tr className="widgetLgTr">
-            <td className="widgetLgUser">
-              <img
-                src="https://i.ibb.co/VH6Hstf/gary-bendig-6-GMq7-AGx-Nb-E-unsplash.jpghttps://i.ibb.co/fS1D105/jonatan-pie-xg-TMSz6keg-E-unsplash.jpghttps://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-                alt=""
-                className="widgetLgImg"
-              />
-              <span className="widgetLgName">Susan Carol</span>
-            </td>
-            <td className="widgetLgDate">2 Jun 2021</td>
-            <td className="widgetLgAmount">$122.00</td>
-            <td className="widgetLgStatus">
-              <Button type="Approved" />
-            </td>
-          </tr>
+          {orders.map((order) => (
+            <tr className="widgetLgTr">
+              <td className="widgetLgUser">
+                <span className="widgetLgName">{order.userId}</span>
+              </td>
+              <td className="widgetLgDate">{format(order.createdAt)}</td>
+              <td className="widgetLgAmount">{order.amount}</td>
+              <td className="widgetLgStatus">
+                <Button type={order.status} />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- src/components/widgetSm/WidgetSm.js
+++ src/components/widgetSm/WidgetSm.js
@@ -1,90 +1,44 @@
-import { Visibility } from "@material-ui/icons";
 import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethod";
 
 export default function WidgetSm() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get("users/?new=true");
+        setUsers(res.data);
+      } catch {}
+    };
+    getUsers();
+    console.log(users);
+  }, []);
+
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
       <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            className="widgetSmImg"
-            src="https://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-            alt=""
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-
-        <li className="widgetSmListItem">
-          <img
-            className="widgetSmImg"
-            src="https://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-            alt=""
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-
-        <li className="widgetSmListItem">
-          <img
-            className="widgetSmImg"
-            src="https://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-            alt=""
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-
-        <li className="widgetSmListItem">
-          <img
-            className="widgetSmImg"
-            src="https://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-            alt=""
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-
-        <li className="widgetSmListItem">
-          <img
-            className="widgetSmImg"
-            src="https://i.ibb.co/yB9wVG1/ricky-kharawala-ad-K3-Vu70-DEQ-unsplash.jpg"
-            alt=""
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
+        {users.map((user) => (
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              className="widgetSmImg"
+              src={
+                users.img ||
+                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+              }
+              alt=""
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
